7b2ca70229aead8ccfc087c0f00a1706ff86dfd5
Carlo Scarioni <carlo.scarioni@gmail.com>
2011-03-29 14:22:26 +0100

    mongo in my server

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,4 +1,4 @@
-MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
+MongoMapper.connection = Mongo::Connection.new('82.165.139.7', 27017)
 MongoMapper.database = "#couchy-#{Rails.env}"
 
 if defined?(PhusionPassenger)
